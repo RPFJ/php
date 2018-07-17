bb9434ee1e32f5ccd57f89d7a09169832fcd756d
Rafael de Paula ferreira de Jesus <raphaelferreiira@gmail.com>
2018-07-16 23:32:35 -0300

    [PHP-03] Aula 48 - Classe Abstrata

---
+++ POO/interface.php
@@ -0,0 +1,32 @@
+<?php
+
+interface Veiculo{
+
+    public function acelerar($velocidade);
+    public function frenar($velocidade);
+    public function trocarMarcha($marcha);
+
+
+}
+
+class Civic implements Veiculo{
+
+    public function acelerar($velocidade){
+        echo "O veícuo acelerou até ". $velocidade . " km/h";
+    }
+
+    public function frenar($velocidade){
+        echo "O veícuo frenou até ". $velocidade . " km/h";
+
+    }
+
+    public function trocarMarcha($marcha){
+        echo "O veícuo trocou a marcha para a  ". $marcha . " marcha";
+    }
+}
+
+$carro = new Civic(); 
+
+$carro->trocarMarcha(1);
+
+?>
